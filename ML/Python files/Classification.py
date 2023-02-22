c747a61e95aa47b0186d2b5990a99851948be3de
Héloïse Feldmann <feldmann.heloise@gmail.com>
2023-02-22 11:57:29 +0100

    cleaning of arborescence

---
+++ ML/Python files/Classification.py
@@ -0,0 +1,64 @@
+import pandas as pd
+import numpy as np
+
+from imblearn.over_sampling import RandomOverSampler
+
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+
+import pickle
+
+
+PATH = "./Cleaned_ML_Unknonw.csv"
+
+
+def open_X_y(path: str):
+    """
+    """
+    # opening the dataset
+    churn = pd.read_csv(path)
+    churn.drop(['Unnamed: 0'], axis=1, inplace=True)
+    # selecting only important features
+    churn = churn[['Attrition_Flag', 'Total_Trans_Ct', 'Total_Revolving_Bal', 'Total_Relationship_Count',
+                  'Total_Amt_Chng_Q4_Q1', 'Total_Trans_Amt', 'Total_Ct_Chng_Q4_Q1', 'Avg_Open_To_Buy', 'Customer_Age']]
+    less = churn.reindex(sorted(churn.columns), axis=1)
+    print(less.columns)
+    #divide to feature and results
+    X = less.drop('Attrition_Flag',axis=1)
+    y = less['Attrition_Flag']
+    print('opened')
+    return X,y
+
+def sampling(X: np.array, y: np.array):
+    """
+    """
+    ros = RandomOverSampler(sampling_strategy="not majority")
+    X_ros, y_ros = ros.fit_resample(X,y)
+    print('sampling')
+    return X_ros,y_ros
+
+def model(X : np.array,y : np.array):
+    """
+    """
+    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+
+    steps=[("scale", StandardScaler()),
+       ("model", DecisionTreeClassifier(random_state=45, max_depth=7,criterion='gini', max_features=None))]
+
+    pipe = Pipeline(steps)
+    print('pipe step')
+    pipe.fit(X_train,y_train)
+    # save as pickle
+    pickle.dump(pipe, open('../Models/Tree_Class.pkl', 'wb'))
+    #print score
+    print(pipe.score(X_test,y_test))
+
+    return pipe 
+
+
+
+X,y = open_X_y(PATH)
+X,y = sampling(X,y)
+model(X,y)
---
+++ ML/Python files/clean_DF.py
@@ -0,0 +1,46 @@
+import pandas as pd
+import numpy as np
+
+# open csv
+churn = pd.read_csv('./BankChurners.csv')
+churned = pd.read_csv('./clusters.csv')
+
+# replace and get rid of nan 
+def clean_nan(data : pd.DataFrame): 
+
+    print(data.shape)
+    data = data.replace(['Unknown'], 0)
+    #data.dropna(inplace=True)
+    print(data.shape)
+
+    print('clean ok')
+    return data
+
+
+def col(data):
+    # get rid of useless coluumns
+    data.rename(columns = {'Naive_Bayes_Classifier_Attrition_Flag_Card_Category_Contacts_Count_12_mon_Dependent_count_Education_Level_Months_Inactive_12_mon_1':'class 1'}, inplace = True)
+    data.rename(columns = {'Naive_Bayes_Classifier_Attrition_Flag_Card_Category_Contacts_Count_12_mon_Dependent_count_Education_Level_Months_Inactive_12_mon_2':'class 2'}, inplace = True)
+    data.drop(['class 1', 'class 2','CLIENTNUM'], axis=1, inplace=True)
+    print('col ok ')
+    return data
+
+def save(data, name):
+    data.to_csv(name)
+    print("saved")
+    return data 
+
+
+def replace(data):
+# replacing categorical values for logical int
+    data = data.replace(['Existing Customer','Married'], 2)
+    data = data.replace(['Attrited Customer','Single'], 1)
+    data = data.replace(['Divorced'], 3)
+    data = data.replace(['Uneducated','High School','College','Graduate','Post-Graduate','Doctorate'], [1,2,3,4,5,6])
+    data = data.replace(['Less than $40K','$40K - $60K','$60K - $80K','$80K - $120K','$120K +'], [1,2,3,4,5])
+    data = data.replace(['Blue','Silver','Gold','Platinum'], [1,2,3,4])
+    data= pd.get_dummies(data, columns=['Gender'])
+    return data 
+
+(save(col(clean_nan(churned)), 'Cleaned_clusters_Unknown.csv')).head()
+#(save(replace(col(clean_nan(churn))), 'Cleaned_ML_Unknonw.csv')).head()
