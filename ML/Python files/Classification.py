43e14ed48dc7a942b36b62ff26f3ea189a107999
Héloïse Feldmann <feldmann.heloise@gmail.com>
2023-02-27 11:47:32 +0100

    final models maybe

--- ML/Python files/Classification.py
+++ ML/Python files/Classification.py
@@ -12,7 +12,7 @@ from sklearn.metrics import classification_report
 import pickle
 
 
-PATH = "./clusters.csv"
+PATH = "./clusters_3.csv"
 
 
 def open_X_y(path: str, class_name : str):
@@ -22,19 +22,15 @@ def open_X_y(path: str, class_name : str):
     churn = pd.read_csv(path)
     churn.drop(['Unnamed: 0'], axis=1, inplace=True)
     # selecting only important features
-    if class_name == 'cluster':
-        churn = churn[['Total_Trans_Ct', 'Total_Revolving_Bal', 'Total_Relationship_Count',
-                    'Total_Amt_Chng_Q4_Q1', 'Total_Trans_Amt', 'Total_Ct_Chng_Q4_Q1', 'Avg_Open_To_Buy', 'Customer_Age', 'cluster']]
-    else : 
-        churn = churn[['Attrition_Flag','Total_Trans_Ct', 'Total_Revolving_Bal', 'Total_Relationship_Count',
+    churn = churn[['Attrition_Flag','Total_Trans_Ct', 'Total_Revolving_Bal', 'Total_Relationship_Count',
                     'Total_Amt_Chng_Q4_Q1', 'Total_Trans_Amt', 'Total_Ct_Chng_Q4_Q1', 'Avg_Open_To_Buy', 'Customer_Age']]
         
     churn = churn.dropna()
     less = churn.reindex(sorted(churn.columns), axis=1)
 
     #divide to feature and results
-    X = less.drop(class_name,axis=1).values
     y = less[class_name].values
+    X = less.drop(class_name,axis=1).values
  
     return X,y
 
@@ -58,33 +54,27 @@ def model(X : np.array,y : np.array,path_model : str):
     """
     """
     X_train, X_test, y_train, y_test = split(X,y)
-
+    # oversampling
     X_train, y_train = sampling(X_train,y_train)
+    # setting up the steps for the pipeline
     steps=[("scale", StandardScaler()),
         ("model", DecisionTreeClassifier(random_state=45, max_depth=6,criterion='gini', max_features=None))]
-
+    # training the model
     pipe = Pipeline(steps)
-
-    pipe.fit(X_train,y_train)
+    pipe.fit(X_train, y_train)
     # save as pickle
     pickle.dump(pipe, open( path_model, 'wb'))
-    # print score
-    y_pred = pipe.predict(X_test)
+    # evaluation and prediction 
     print(pipe.score(X_test,y_test))
-    #print(classification_report(y_test, y_pred))
     # test of prediction
     new_X = (np.array([20, 100, 30, 500, 50, 2.500, 20, 40])).reshape(-1, 8)
-    predict = pipe.predict(new_X).tolist()
     proba = pipe.predict_proba(new_X).tolist()
     proba = proba[0][0]
     print(f"The customer has a {round(proba*100)} % chance of churning")
 
-    #print(f"It is an {predict} and the probability is {proba}")
-
     return pipe
 
 chrun_model = '../Models/Churn_class.pkl'
-cluster_model = '../Models/Clusters_Class.pkl'
 
 X,y = open_X_y(PATH, 'Attrition_Flag')
 model(X,y, chrun_model)
--- ML/Python files/Clustering.py
+++ ML/Python files/Clustering.py
@@ -3,25 +3,28 @@ from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.pipeline import Pipeline
 from sklearn.cluster import KMeans
 from sklearn.decomposition import PCA
+import pickle
 
 import pandas as pd
 
 PATH = './Cleaned_ML_Unknonw.csv'
 
-def open(PATH : str):
+def open_df(PATH : str):
     """
     """
     data = pd.read_csv(PATH)
+    data.drop(['Unnamed: 0'], axis=1, inplace=True)
+
     return data
 
 
-def pipeline(data : pd.DataFrame):
+def pipeline(data : pd.DataFrame, path_model: str):
     """
     """
-    cat_cols = ['Gender', 'Education_Level','Card_Category', 'Income_Category']
+    # separate categorical column and numerical for preprocess
+    cat_cols = ['Gender', 'Education_Level','Card_Category', 'Income_Category','Attrition_Flag','Marital_Status']
     int_cols = data.drop(cat_cols, axis=1).columns
-    print(int_cols)
-
+    # preprocessing step for the pipeline
     preprocess = ColumnTransformer(
         [
         ("Scaling", StandardScaler(), int_cols),
@@ -30,20 +33,22 @@ def pipeline(data : pd.DataFrame):
     )
 
     model = KMeans(n_clusters=3,n_init=10,init='k-means++')
-
+    # pipeline 
     steps = [
         ("preprocess" , preprocess),
-        ("model", model),
-        ("PCA" , PCA(n_components=20))
+        ("PCA" , PCA(n_components=20)),
+        ("model", model)
     ]
     pipe = Pipeline(steps)
-
-    cluster_labels = pd.Series(pipe.labels_, name='cluster')
+    # fitting the model
+    pipe.fit(data)
+    pickle.dump(pipe, open( path_model, 'wb'))
+    # Adding a cluster col to a new dataframe
+    cluster_labels = pd.Series(model.labels_, name='cluster')
     data = data.join(cluster_labels.to_frame())
-    
-    data.to_csv('clusters.csv')
-    print(data.head())
+    data.to_csv('./clusters_3.csv')
 
-    pipe
+# sepcifying path for the pickle model
+clustering_model = '../Models/Churn_cluster_3.pkl'
 
-pipeline(open(PATH))
+pipeline(open_df(PATH), clustering_model)
